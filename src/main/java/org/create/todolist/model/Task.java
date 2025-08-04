8591e8b8b31ff3e25043089a6508ba492dd42172
Dominika <dominika.jasinska.lublin@gmail.com>
2025-08-04 20:17:34 +0200

    Add Task and Status of a task the model

---
+++ src/main/java/org/create/todolist/model/Task.java
@@ -0,0 +1,15 @@
+package org.create.todolist.model;
+
+import java.time.LocalDate;
+
+
+public class Task {
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private Status status;
+
+
+
+
+}
