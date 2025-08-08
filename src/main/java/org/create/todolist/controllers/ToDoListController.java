4396542f9faa7e627fc92f011b31505b379cd296
Dominika <dominika.jasinska.lublin@gmail.com>
2025-08-08 15:57:07 +0200

    Mark task as completed, mark all tasks as completed (completed = true after clicking on the checkbox)

--- src/main/java/org/create/todolist/controllers/ToDoListController.java
+++ src/main/java/org/create/todolist/controllers/ToDoListController.java
@@ -15,18 +15,44 @@ public class ToDoListController {
     @Autowired
     private TaskRepository taskRepository;
 
-    @PostMapping("/to-do-list")
+    @GetMapping("/to-do-list")
+    public String getTasks(Model model) {
+        List<Task> tasks = taskRepository.findAll();
+        model.addAttribute("tasks", tasks);
+        return "to-do-list";
+    }
+
+    @PostMapping(value = "/to-do-list", params = {"!markAllCompleted", "!id"})
     public String saveTask(@RequestParam String title,
                            @RequestParam(required = false) String description) {
         taskRepository.save(new Task(title, description));
         return "redirect:/";
     }
 
-    @GetMapping("/to-do-list")
-    public String getTasks(Model model) {
+    @PostMapping(value = "/to-do-list", params = "id")
+    public String updateTask(@RequestParam Long id,
+                             @RequestParam(required = false) String completed) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Not found"));
+
+        task.setCompleted(completed != null); //
+        taskRepository.save(task);
+
+        return "redirect:/to-do-list";
+    }
+
+    @PostMapping(value = "/to-do-list", params = "markAllCompleted")
+    public String markAllTasksCompleted() {
         List<Task> tasks = taskRepository.findAll();
-        model.addAttribute("tasks", tasks);
-        return "to-do-list";
+
+        if (tasks.isEmpty()) {
+            return "redirect:/to-do-list";
+        }
+
+        tasks.forEach(task -> task.setCompleted(true));
+        taskRepository.saveAll(tasks);
+
+        return "redirect:/to-do-list";
     }
 
 
