040523bbe39e5fc080c72690f776c773ef47b3dc
Dominika <dominika.jasinska.lublin@gmail.com>
2025-08-18 20:21:54 +0200

    DownloadPdf test, App tests

--- src/test/java/org/create/todolist/ToDoListApplicationTests.java
+++ src/test/java/org/create/todolist/ToDoListApplicationTests.java
@@ -1,13 +1,37 @@
 package org.create.todolist;
 
+import org.create.todolist.controllers.ToDoListController;
+import org.create.todolist.data.TaskRepository;
+import org.create.todolist.model.Task;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ToDoListApplicationTests {
 
+    @Autowired
+    private ToDoListController taskController;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
     @Test
     void contextLoads() {
+        assertNotNull(taskController);
+        assertNotNull(taskRepository);
     }
+
+    @Test
+    void shouldSaveTaskToDatabase() {
+        Task task = new Task("Test", "description");
+        Task saved = taskRepository.save(task);
+        assertNotNull(saved.getId());
+    }
+
+
 
 }
--- src/test/java/org/create/todolist/controllers/DownloadControllerTest.java
+++ src/test/java/org/create/todolist/controllers/DownloadControllerTest.java
@@ -1,11 +1,55 @@
 package org.create.todolist.controllers;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import org.create.todolist.model.Task;
+import org.create.todolist.data.TaskRepository;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class DownloadControllerTest {
 
-    @Test
-    void exportToDoListPdf() {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @BeforeEach
+    void setup() {
+        taskRepository.deleteAll();
     }
+
+    @Nested
+    class ExportToDoListPdf{
+        @Test
+        @DisplayName("shouldGeneratePdfWithTasks")
+        void shouldGeneratePdfWithTasks() throws Exception {
+            taskRepository.save(new Task("Read a book", "your favourite"));
+            taskRepository.save(new Task("Go for a walk", "30 minutes"));
+
+            MvcResult result = mvc.perform(get("/to-do-list/pdf"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_PDF))
+                    .andExpect(header().string(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"todolist.pdf\""))
+                    .andReturn();
+
+            byte[] pdfBytes = result.getResponse().getContentAsByteArray();
+            Assertions.assertTrue(pdfBytes.length > 0);
+
+        }
+    }
+
+
+
 }
